172d4161e741bb946eba693769690ff21cd5cc67
joseftango <4279@holbertonschool.com>
2023-10-11 21:58:40 +0100

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""5-sum_list module"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """function sum_list """
+    return sum(x for x in input_list)
