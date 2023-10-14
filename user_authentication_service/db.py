cd980dc0d960b5ba20419859b0cb5f704746ff34
joseftango <4279@holbertonschool.com>
2023-10-14 19:42:53 +0100

    db.py

---
+++ user_authentication_service/db.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" module named DB"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+from typing import TypeVar
+from user import Base
+from user import User
+
+
+class DB:
+    """class DB"""
+
+    def __init__(self) -> None:
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Adds user to database"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """Finds user by key word"""
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+        
+
+        return user
+    
+    
+
+    
