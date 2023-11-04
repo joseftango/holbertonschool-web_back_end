2613f9df9d178999f1218546660341da1485a78e
joseftango <4279@holbertonschool.com>
2023-11-04 14:45:03 +0100

    exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """exercice module"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -16,3 +16,26 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """take a key string argument and an optional Callable"""
+        my_key = self._redis.get(key)
+        if my_key is not None:
+            if fn is not None:
+                return fn(my_key)
+            else:
+                return my_key
+        return None
+
+    def get_str(self, key: str) -> str:
+        """get str function"""
+        return self._redis.get(key).decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """get int function"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
