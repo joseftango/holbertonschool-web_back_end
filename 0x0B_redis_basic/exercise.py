9878e79585295a6c7ecfaaab8e63403b99d409fa
joseftango <4279@holbertonschool.com>
2023-11-04 13:38:33 +0100

    exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """exercice module"""
 import redis
 import uuid
+from typing import Union
 
 
 class Cache():
@@ -10,7 +11,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """method that takes a data argument and returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
