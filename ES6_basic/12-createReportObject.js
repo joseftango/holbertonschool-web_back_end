6f47823d7ff91ce5626a30a257284dfb63f02879
joseftango <4279@holbertonschool.com>
2023-11-16 02:55:09 +0100

    12-createReportObject.js

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,6 +1,8 @@
 export default function createReportObject(employeesList) {
-  const myObj = {};
-  myObj.allEmployees = { ...employeesList };
-  myObj.getNumberOfDepartments = () => Object.keys(myObj).length;
-  return myObj;
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
