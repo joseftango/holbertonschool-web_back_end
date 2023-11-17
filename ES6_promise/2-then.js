60a832d4dd5844e6d199f56a4a2d839088b1fb4a
joseftango <4279@holbertonschool.com>
2023-11-17 17:25:38 +0100

    2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-	return promise.then((resolveVal) => ({status: 200 ,body: success}))
-	.catch((rejectVal) => Error())
-	.finally(console.log('Got a response from the API'))
+  return promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(console.log('Got a response from the API'));
 }
