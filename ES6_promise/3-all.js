049e49bbebdf223b0d2c707e70eb7f2e0095c469
joseftango <4279@holbertonschool.com>
2023-11-18 14:38:16 +0100

    3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,18 @@
+// 3-all.js
+
 import { createUser, uploadPhoto } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const { body } = values[0];
-      const { firstName } = values[1];
-      const { lastName } = values[1];
+      const { firstName, lastName } = values[1];
+
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
+
+export default handleProfileSignup;
