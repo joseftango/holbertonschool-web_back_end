319ae1c59640b28b5b1c7b3990355df1d3a754b2
joseftango <4279@holbertonschool.com>
2023-11-18 16:13:25 +0100

    6-final-user.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+	const userPro = signUpUser(firstName, lastName);
+	const photoRej = uploadPhoto(fileName);
+	return Promise.allSettled([userPro, photoRej]);
+}
