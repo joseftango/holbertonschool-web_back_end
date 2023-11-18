372d638a67a8df16c2e435ddb26f696923eccabd
joseftango <4279@holbertonschool.com>
2023-11-18 18:42:19 +0100

    2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -18,21 +18,21 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof value !== 'string') {
+    if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
 
   set length(newLength) {
-    if (typeof value !== 'number') {
+    if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = newLength;
   }
 
   set students(newStudents) {
-    if (!Array.isArray()) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = newStudents;
