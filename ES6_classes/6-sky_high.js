23067397916dd72c9f4561d4cf278edcbd5ccfbe
joseftango <4279@holbertonschool.com>
2023-11-20 15:18:23 +0100

    6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -10,22 +10,8 @@ export default class SkyHighBuilding extends Building {
     return this._sqft;
   }
 
-  set sqft(newSqft) {
-    if (this._sqft !== 'number') {
-      throw new TypeError('newSqft must be a number');
-    }
-    this._sqft = newSqft;
-  }
-
   get floors() {
     return this._floors;
-  }
-
-  set floors(newFloores) {
-    if (newFloores !== 'number') {
-      throw new TypeError('newFloores must be a number');
-    }
-    this._floors = newFloores;
   }
 
   evacuationWarningMessage() {
