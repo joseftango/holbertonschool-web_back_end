9b4f7484d0f90da5640826a1ab999437b2107627
joseftango <4279@holbertonschool.com>
2023-11-20 14:49:19 +0100

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,14 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building
+    && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  static evacuationWarningMessage() {
-    throw new Error('evacuationWarningMessage must be implemented in derived class');
   }
 }
