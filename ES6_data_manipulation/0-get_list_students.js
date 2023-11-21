e0518eca58461b8586c874e979313781f94dbd26
joseftango <4279@holbertonschool.com>
2023-11-21 11:27:26 +0100

    0-get_list_students.js

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const ob1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const ob2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const ob3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  const res = [ob1, ob2, ob3];
+  return res;
+}
