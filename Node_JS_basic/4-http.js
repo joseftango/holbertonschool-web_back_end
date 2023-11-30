d1228d6ab72e325545fbbe3f8c0cea71bb936efd
joseftango <4279@holbertonschool.com>
2023-11-30 01:03:07 +0100

    4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 const port = 1245;
+
 const app = http.createServer((req, res) => {
-  if (req.url === '/' || req.url === '/any_endpoint') {
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello Holberton School!');
-    res.end();
-  }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 app.listen(port, () => {
