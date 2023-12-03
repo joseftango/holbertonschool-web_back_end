221c2e2d0126aadca778333a18e5a0963b62ba96
joseftango <4279@holbertonschool.com>
2023-12-03 21:55:12 +0100

    5-payment.js

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const expect = require('chai').expect;
+
+
+describe('getPaymentTokenFromAPI', function() {
+    it('async testing Promise', (done) => {
+        getPaymentTokenFromAPI(true)
+            .then((res) => {
+                expect(res).to.eql({ data: 'Successful response from the API' })
+            })
+        done();
+    })
+})
