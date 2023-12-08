8cc5e2c215280dd5de225c7e620cbe2b4d04d13d
joseftango <4279@holbertonschool.com>
2023-12-08 22:03:28 +0100

    6-job_processor.js

---
+++ queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,17 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
+
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+
+  sendNotification(phoneNumber, message);
+
+  done();
+});
+
+console.log('Job processor is running...');
