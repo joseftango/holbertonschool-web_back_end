e9264686e85baafb3fa987f880002d0270934791
joseftango <4279@holbertonschool.com>
2024-05-23 18:01:09 +0100

    web.py

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -6,30 +6,30 @@ import requests
 from typing import Callable
 from functools import wraps
 
-rd = redis.Redis()
+r = redis.Redis()
 
 
-def count_requests(method: Callable) -> Callable:
-    """ Counting the number of requests done"""
+def count_calls(method: Callable) -> Callable:
+    """ Counting the number of requests done """
 
     @wraps(method)
     def wrapper(url):
         """ Wrapper for function behavior """
-        rd.incr(f"count:{url}")
-        cached_html = rd.get(f"cached:{url}")
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
 
         html = method(url)
-        rd.setex(f"cached:{url}", 10, html)
+        r.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
 
 
-@count_requests
+@count_calls
 def get_page(url: str) -> str:
     """ requests module to obtain the HTML
-        content of a specific URL and returns it."""
+    content of a specific URL and returns it."""
     req = requests.get(url)
     return req.text
