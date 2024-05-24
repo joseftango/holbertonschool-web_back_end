30e5b76191f794783741e5d3d8223a5d7cd375b7
joseftango <4279@holbertonschool.com>
2024-05-24 12:00:32 +0100

    7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,18 +3,16 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 const port = 1245;
-const hostname = '127.0.0.1';
-const filePath = process.argv[2];
-const promise = countStudents(filePath);
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', async (req, res) => {
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
 });
-
-app.get('/students', (req, res) => {
-  promise.then((data) => {
-    res.send(`This is the list of our students\n${data}`);
-  }).catch(() => { res.send('This is the list of students is not available'); });
-});
-
-module.exports = app.listen(port, hostname);
+app.listen(port);
+module.exports = app;
