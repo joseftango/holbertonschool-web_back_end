7dd6800cc543f56c880af1fab10b6c59fc8945d5
joseftango <4279@holbertonschool.com>
2025-06-11 20:50:10 +0100

    6-sum_mixed_list.py

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""6-sum_mixed_list"""
+"""6-sum_mixed_list module"""
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''calculates the sum of all int or float items in list'''
     return sum(mxd_lst)
