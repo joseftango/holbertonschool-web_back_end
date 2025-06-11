1acc44c2dade069c1ecda8120b81d540a042c99c
joseftango <4279@holbertonschool.com>
2025-06-11 21:52:27 +0100

    8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,5 +4,7 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """takes a float multiplier as argument and returns
+    a function that multiplies a float by multiplier"""
     x: float = 2.22
     return lambda multiplier: x*multiplier
