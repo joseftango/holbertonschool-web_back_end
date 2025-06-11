61cce88e0b8166c12551d77f6d402385a04e875d
joseftango <4279@holbertonschool.com>
2025-06-11 21:19:44 +0100

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[int, str]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """creates a tuple of key value"""
     return k, v**2
