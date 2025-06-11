1b919991c004607165de2011b67d112eb9e8c660
joseftango <4279@holbertonschool.com>
2025-06-11 20:35:59 +0100

    5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """5-sum_list module"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
-    '''function that add all number in the list'''
+def sum_list(input_list: List[float]) -> float:
+    '''function that add all float numbers in the list'''
     return sum(input_list)
