814fb25af0ab70bba9a0d87bc227864fb9bee6ef
joseftango <4279@holbertonschool.com>
2025-06-14 23:33:03 +0100

    2-measure_runtime.py

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python3
 """2-measure_runtime module"""
 import asyncio
-import random
+import typing
 import time
-from typing import List
-
-
-async def wait_random(max_delay: int = 10) -> float:
-    """wait_random"""
-    delay = random.uniform(0, float(max_delay))
-    await asyncio.sleep(delay)
-    return delay
-
-
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """wait_n"""
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    results = await asyncio.gather(*tasks)
-    return sorted(results)
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """measure_time function"""
-    start_time = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    return elapsed_time / n
+    """measures the total execution"""
+    start: float = time.time()
+    res: typing.List[float] = asyncio.run(wait_n(n, max_delay))
+    end: float = time.time()
+
+    total_time: float = end - start
+
+    return total_time / n
