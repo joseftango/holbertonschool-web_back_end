3c27b8e2c25e7257af4868f78c168584f3912028
joseftango <4279@holbertonschool.com>
2025-07-25 14:50:30 +0100

    1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
+
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
+
   if (trueOrFalse) {
-    /* eslint-disable */
-    const task = true;
-    const task2 = false;
+    let task = true;
+    let task2 = false;
   }
+
   return [task, task2];
 }
