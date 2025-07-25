ca1f1de877214fc381ab9d9b503d450491010e08
joseftango <4279@holbertonschool.com>
2025-07-25 14:28:24 +0100

    0-constants.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,6 +1,6 @@
-export function taskFirst() {
-  const task = 'I prefer const when I can.';
 
+export const taskFirst = () => {
+  var task = 'I prefer const when I can.';
   return task;
 }
 
@@ -8,8 +8,8 @@ export function getLast() {
   return ' is okay';
 }
 
-export function taskNext() {
-  let combination = 'But sometimes let';
+export let taskNext = () => {
+  var combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
